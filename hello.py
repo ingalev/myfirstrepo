34e54765c02b9db51c00a5d843461e352fc315ca
ingalev <115534735+ingalev@users.noreply.github.com>
2022-10-12 18:16:27 +0530

    Update hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+def swap(a,b):
+    a,b=b,a
+    print('updated value:-',a)
+    print('updated value:-',b)
+a=int(input('enter the value of a='))
+b=int(input('enter the value of b=')) 
+swap(a,b)   
